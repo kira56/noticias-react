6d22b98e207f79c4fac464e460ede6216c316391
kira56 <ridhunter1508@gmail.com>
2020-08-02 22:12:17 -0500

    noticias commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
 
 function App() {
+  // Definir la categoria
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `http://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=354aae5250be4b3da5ade6b52103c6ab`;
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles)
+    }
+    consultarAPI();
+  }, [categoria])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header
+        titulo='Buscador de Noticias'
+      />
+      <div className="container white">
+        <Formulario
+          guardarCategoria={guardarCategoria}
+        />
+
+        <ListadoNoticias 
+          noticias={noticias}
+        />
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/hooks/useSelect.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const useSelect = (stateInicial, opciones) => {
+
+    // state del customhook
+    const [state, actualizarState] = useState(stateInicial);
+    const SelectNoticias = () => (
+
+        <select className="browser-default" value={state} onChange={e => actualizarState(e.target.value)}>
+            {opciones.map(opcion => (
+                <option key={opcion.value} value={opcion.value}>{opcion.label}</option>
+            ))}
+        </select>
+    );
+    return [state, SelectNoticias]
+}
+export default useSelect;
